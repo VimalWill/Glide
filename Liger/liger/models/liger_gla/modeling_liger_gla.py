5acf795ea404fefa77ae2714bab05cde722e2e64
VimalWill <vimalwilliam99@gmail.com>
2026-02-03 08:30:47 +0530

    updated the api and error fix

--- Liger/liger/models/liger_gla/modeling_liger_gla.py
+++ Liger/liger/models/liger_gla/modeling_liger_gla.py
@@ -35,7 +35,7 @@ else:
     print("flash_attn_2 is not available")
 
 from fla.models.utils import Cache as FlaCache
-from fla.ops.gla import chunk_gla, recurrent_gla
+from fla.ops.gla import chunk_gla, fused_recurrent_gla
 
 from .configuration_liger_gla import LigerGLAConfig
 
@@ -118,7 +118,7 @@ class LigerGatedLinearAttention(nn.Module):
         if self.training or q.shape[-2] > 1:
             o_, recurrent_state = chunk_gla(q=q, k=k, v=v, g=g, scale=scale, initial_state=recurrent_state, output_final_state=True)
         else:
-            o_, recurrent_state = recurrent_gla(q=q, k=k, v=v, g=g, scale=scale, initial_state=recurrent_state, output_final_state=True)
+            o_, recurrent_state = fused_recurrent_gla(q=q, k=k, v=v, g=g, scale=scale, initial_state=recurrent_state, output_final_state=True)
 
         if past_key_value is not None:
             past_key_value.update(
