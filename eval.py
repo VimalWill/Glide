dda694942475b8a1b81625a3dcbadf8a2c221c6d
VimalWill <vimalwilliam99@gmail.com>
2026-02-19 07:01:50 +0530

    reduced the batch size

--- eval.py
+++ eval.py
@@ -22,7 +22,7 @@ TASKS = [
 NORM_TASKS = {"arc_challenge", "hellaswag"}
 
 
-def _eval_model(model: GlideForCausalLM, tokenizer, batch_size: int = 8):
+def _eval_model(model: GlideForCausalLM, tokenizer, batch_size: int = 1):
     lm = HFLM(pretrained=model, tokenizer=tokenizer, batch_size=batch_size)
 
     with torch.no_grad():
